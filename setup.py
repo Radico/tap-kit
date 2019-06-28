46c498a655043b15113d68cfaefdcd776be0920d
imcinerney <ian@simondata.com>
2019-06-28 15:08:48 -0400

    Revert "Updated pendulum to 2.0"
    This reverts commit d70356b787bef9ca7d3aa5b358ff4b22f333307d.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "singer-python==5.2.0",
         'requests==2.18.4',
-        "pendulum==2.0.4",
+        "pendulum==1.2.0",
     ],
     packages=["tap_kit"],
     include_package_data=True,
