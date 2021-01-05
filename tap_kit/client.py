64eda1eedf99fede531e68dad4c8542302fd4f19
morgan <mmattone@simondata.com>
2021-01-05 10:18:38 -0500

    raise error properly

--- tap_kit/client.py
+++ tap_kit/client.py
@@ -62,8 +62,7 @@ class BaseClient:
 
 
         if response.status_code in RATE_LIMIT_ERROR_CODES + BAD_REQUEST_CODES:
-            # response.raise_for_status() testing response error
-            raise RetryableException()
+            raise RetryableException(response)
 
         response.raise_for_status()
 
